4b330dd3464d8c30faf9bdc252b9004805d8e54a
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-04-13 10:26:57 +0500

    REgistrating dependecy injectoin with services and repositories and configration.

--- Market.Web/Program.cs
+++ Market.Web/Program.cs
@@ -1,7 +1,27 @@
+using Market.Data.DbContexts;
+using Market.Data.IRepositories;
+using Market.Data.Repositories;
+using Market.Service.Interfaces;
+using Market.Service.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// reigster dbcontext to DI conteiner 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Database not found or cannot be accessed"),b => b.MigrationsAssembly("Market.Web")));
+
+// registrate repository
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+// registrate Service
+builder.Services.AddScoped<IProductService, ProductService>();
+
 
 var app = builder.Build();
 
